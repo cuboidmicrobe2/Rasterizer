905fefad608f5d8bec7221063736935909ca8638
Sebastian Einarsson <sebastian.einarsson03@gmail.com>
2025-01-20 16:05:15 +0100

    added matrices

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -1,10 +1,12 @@
 struct PixelShaderInput
 {
     float4 position : SV_POSITION;
-    float3 color : COLOR;
+    float4 worldPosition : WORLD_POSITION;
+    float3 normal : NORMAL;
+    float2 uv : UV;
 };
 
 float4 main(PixelShaderInput input) : SV_TARGET
 {
-    return float4(input.color, 1.0f);
+    return float4(abs(input.normal), 1.0f);
 }
--- VertexShader.hlsl
+++ VertexShader.hlsl
@@ -1,19 +1,30 @@
+cbuffer ConstantBuffer : register(b0)
+{
+    float4x4 worldMatrix;
+    float4x4 viewProjectionMatrix;
+};
+
 struct VertexShaderInput
 {
     float3 position : POSITION;
-    float3 color : COLOR;
+    float3 normal : NORMAL;
+    float2 uv : UV;
 };
 
 struct VertexShaderOutput
 {
     float4 position : SV_POSITION;
-    float3 color : COLOR;
+    float4 worldPosition : WORLD_POSITION;
+    float3 normal : NORMAL;
+    float2 uv : UV;
 };
 
 VertexShaderOutput main(VertexShaderInput input)
 {
     VertexShaderOutput output;
-    output.position = float4(input.position, 1.0f);
-    output.color = input.color;
+    output.worldPosition = mul(float4(input.position, 1.0f), worldMatrix);
+    output.position = mul(output.worldPosition, viewProjectionMatrix);
+    output.normal = normalize(float4(mul(float4(input.normal, 1.0f), worldMatrix).xyz, 0.0f));
+    output.uv = input.uv;
     return output;
 }
