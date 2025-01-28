e876e5600539fbbb2a090c61c4ec973c9fa9ef38
Sebastian Einarsson <sebastian.einarsson03@gmail.com>
2025-01-28 20:27:13 +0100

    math wasn't mathing

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -14,23 +14,23 @@ struct PixelShaderInput
 {
     float4 position : SV_POSITION;
     float4 worldPosition : WORLD_POSITION;
-    float3 normal : NORMAL;
+    float4 normal : NORMAL;
     float2 uv : UV;
 };
 
 float4 main(PixelShaderInput input) : SV_TARGET
-{    
-    float3 normalizedNormal = normalize(input.normal);
-    float3 lightDirection = normalize(lightPosition.xyz - input.worldPosition.xyz);
+{
+    float4 normalizedNormal = normalize(input.normal);
+    float4 lightDirection = normalize(lightPosition - input.worldPosition);
     float diffuseIntensity = max(dot(normalizedNormal, lightDirection), 0.0f);
     
-    float3 reflection = reflect(-lightDirection, normalizedNormal);
-    float3 vectorToCamera = normalize(cameraPosition - input.worldPosition);
+    float4 reflection = reflect(-lightDirection, normalizedNormal);
+    float4 vectorToCamera = normalize(cameraPosition - input.worldPosition);
     float specularIntensity = pow(max(dot(reflection, vectorToCamera), 0.0f), shininess);
     
     float4 ambientComponent = lightColor * ambientLightIntensity;
     float4 diffuseComponent = lightColor * diffuseIntensity;
     float4 specularComponent = lightColor * specularIntensity;
 
-    return (ambientComponent + diffuseComponent + specularComponent) * shaderTexture.Sample(samplerState, input.uv);
+    return (ambientComponent + diffuseComponent) * shaderTexture.Sample(samplerState, input.uv) + specularComponent;
 }
